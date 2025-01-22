36e82b71eed7f67ea19ef21978baa2a4d9865508
shet14886@gmail.com <shet14886@gmail.com>
2025-01-22 23:30:04 +0900

    visual map build updated.

--- MyWrom.cpp
+++ MyWrom.cpp
@@ -11,6 +11,7 @@ enum Object {
     Snake = 1,
     Apple = 2
 };
+constexpr char object_char[] = {'.', 'S', 'A'};
 
 enum Direction {
     Left,
@@ -20,15 +21,15 @@ enum Direction {
 };
 
 Object map[MAP_SIZE];
-int snake_pos[MAP_SIZE];
+int snake_pos[MAP_SIZE] = {MAP_SIZE / 2, };
 
-std::string visual_map;
+std::string visual_map(MAP_SIZE + GAME_SIZE, '\n');
 
 int snake_size = 1;
 
 Direction current_dir;
 
-int apple_pos;
+int apple_pos = 0;
 
 std::random_device random_device;
 std::mt19937 random_generator;
@@ -36,13 +37,12 @@ std::mt19937 random_generator;
 std::vector<int> available_pos;
 
 void initialize() {
-    visual_map.reserve(MAP_SIZE + GAME_SIZE);
     for(int i=GAME_SIZE; i<MAP_SIZE; i+=GAME_SIZE) {
+        visual_map[i-1] = 'd';
         visual_map[i] = '\n';
     }
 
     random_generator = std::mt19937(random_device());
-    available_pos.reserve(MAP_SIZE);
 }
 
 int move(int origin, Direction dir) {
@@ -73,7 +73,7 @@ bool is_valid_move(int origin, int target, Direction dir) {
     {
         case Left:
         case Right:
-            result = (origin%GAME_SIZE == target%GAME_SIZE);
+            result = (origin/GAME_SIZE == target/GAME_SIZE);
             break;
         case Up:
         case Down:
@@ -90,6 +90,8 @@ int move_head(Direction dir) {
     if(!is_valid_move(snake_pos[0], target, dir)) {
         return -1;
     }
+
+    snake_pos[0] = target;
 
     return 0;
 }
@@ -143,13 +145,24 @@ void build_map() {
     map[apple_pos] = Apple;
 }
 
+void build_visual_map() {
+    for(int i=0; i<MAP_SIZE; i++) {
+        visual_map[i + i/GAME_SIZE] = object_char[map[i]];
+    }
+}
+
 void update() {
     move_head(current_dir);
     check_apple();
     track_head(1, snake_pos[0]);
     build_map();
+    build_visual_map();
 }
 
 int main() {
     initialize();
+    update();
+    std::cout << visual_map;
+    system("pause");
+    return 0;
 }
